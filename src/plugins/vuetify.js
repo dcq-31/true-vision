a180d5654af0254e6507e888994e75599c227972
dcq <dariancapoteq@gmail.com>
2022-10-30 17:40:47 -0400

    Custom color palette

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#9425a7',
+        secondary: '#00b4cc',
+      }
+    }
+  }
 });
