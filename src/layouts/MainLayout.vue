f8b6a1d53aef58d701ac9fd455f385244ae59f40
dcq <dariancapoteq@gmail.com>
2022-11-05 08:49:57 -0400

    Create simple MainLayout and MainFooter

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-main class="grey lighten-3">
+    <AppBarMain app />
+    <router-view />
+    <FooterMain app />
+  </v-main>
+</template>
+
+<script>
+import AppBarMain from "/src/components/layout/AppBarMain";
+import FooterMain from "/src/components/layout/FooterMain";
+
+export default {
+  name: "MainLayout",
+  components: {
+    AppBarMain,
+    FooterMain,
+  },
+};
+</script>
