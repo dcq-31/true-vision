fbb1fb1dc82b10ca3f1e0688654c9875022e3a5c
dcq <dariancapoteq@gmail.com>
2022-11-06 00:25:11 -0500

    Add header brand and improve design

--- src/components/layout/AppBarMain.vue
+++ src/components/layout/AppBarMain.vue
@@ -1,27 +1,51 @@
 <template>
-  <v-app-bar class="white" fixed>
-    <v-tabs v-model="tabModel" color="primary" centered>
+  <v-app-bar fixed elevate-on-scroll class="white">
+    <v-toolbar-title class="ml-5 font-weight-black text-h6"
+      >True Vision</v-toolbar-title
+    >
+
+    <v-spacer />
+    <!-- Navigation tabs -->
+    <v-tabs
+      align-with-title
+      v-model="tab"
+      color="primary"
+      style="width: auto"
+      right
+      optional
+    >
+      <!-- TODO: change tabModel when scroll -->
       <v-tab
         v-for="(link, index) in navLinks"
         :key="`app-bar-main-navlink-${index}`"
-        class="black--text text-body-1"
-        @click="$vuetify.goTo(link.to)"
+        class="grey--text text--darken-4 text-body-1 font-weight-bold"
+        :to="link.to"
       >
         {{ link.title }}
       </v-tab>
     </v-tabs>
-    <v-btn class="mx-2 font-weight-bold" color="primary" dark> Entrar </v-btn>
+    <!-- / Navigation tabs -->
+
+    <v-btn
+      class="mx-2 font-weight-bold text-body-1"
+      color="primary"
+      dark
+      :to="{ name: ROUTES.LOGIN }"
+    >
+      <v-icon class="mr-2">mdi-login</v-icon> Acceder
+    </v-btn>
   </v-app-bar>
 </template>
  
  <script>
+import { ROUTES } from "/src/router/names";
 export default {
   name: "AppBarMain",
   data: () => ({
-    tabModel: 0,
+    tab: null,
     navLinks: [
       {
-        title: "Nuestros Servicios",
+        title: "Servicios",
         to: "#service",
       },
       {
@@ -29,10 +53,14 @@ export default {
         to: "#about",
       },
       {
-        title: "Contáctanos",
+        title: "Contacto",
         to: "#contact",
       },
     ],
+    options: {
+      duration: 600,
+    },
+    ROUTES: ROUTES,
   }),
 };
 </script>
